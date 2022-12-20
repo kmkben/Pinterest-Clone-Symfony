b4305171ca50639c211fe774748acb94a8649629
Kemoko KEITA <kemokokeita710@gmail.com>
2022-12-20 20:53:18 +0000

    Adding fields createdAt and updatedAt

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -3,11 +3,13 @@
 namespace App\Entity;
 
 use App\Repository\PinRepository;
+use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PinRepository::class)]
 #[ORM\Table(name:"pins")]
+#[ORM\HasLifecycleCallbacks]
 class Pin
 {
     #[ORM\Id]
@@ -20,6 +22,12 @@ class Pin
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
+
+    #[ORM\Column(options:["default" => "CURRENT_TIMESTAMP"])]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(options:["default" => "CURRENT_TIMESTAMP"])]
+    private ?\DateTimeImmutable $updateAt = null;
 
     public function getId(): ?int
     {
@@ -48,5 +56,40 @@ class Pin
         $this->description = $description;
 
         return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeImmutable
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(\DateTimeImmutable $updateAt): self
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+    #[ORM\PrePersist]
+    #[ORM\PreUpdate]
+    public function updateTimestamp( )
+    {
+        if ($this->getCreatedAt() === null)
+        {
+            $this->setCreatedAt(new \DateTimeImmutable());
+        }
+        $this->setUpdateAt(new \DateTimeImmutable());
     }
 }
